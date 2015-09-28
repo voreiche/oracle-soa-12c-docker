df47af1211ad4c8ccb12e25f15200a6e6a3cae1d
voreiche <volker@vritb.de>
2015-09-28 16:18:20 +0200

    config changes, don’t run docker-push, use docker-tag flag forced

--- samples/12c-domain/container-scripts/create-soa-domain.py
+++ samples/12c-domain/container-scripts/create-soa-domain.py
@@ -1,3 +1,4 @@
+CONFIG_JVM_ARGS='-Djava.security.egd=file:/dev/./urandom'
 DOMAIN_MODE='Compact'
 DOMAIN_NAME='soa_domain'
 DOMAIN_PATH='/u01/oracle/work/domains/' + DOMAIN_NAME
@@ -15,6 +16,8 @@ print 'Creating Base Domain'
 
 readTemplate('/u01/oracle/soa/wlserver/common/templates/wls/wls.jar', DOMAIN_MODE)
 
+print 'Template wls read.'
+
 setOption('ServerStartMode', 'dev')
 setOption('AppDir', APP_PATH)
 
@@ -22,17 +25,26 @@ cd('/Security/base_domain/User/weblogic')
 set('Name', ADMIN_USERNAME)
 cmo.setPassword(ADMIN_PASSWORD)
 
+print 'writing Domain' 
+print DOMAIN_PATH
 writeDomain(DOMAIN_PATH)
+
+print 'writeDomain done.'
+
 closeTemplate()
 
 print 'Adding Templates'
 
 readDomain(DOMAIN_PATH)
 
+print 'readDomain done'
+
 addTemplate('/u01/oracle/soa/osb/common/templates/wls/oracle.osb_template_12.1.3.jar')
 addTemplate('/u01/oracle/soa/soa/common/templates/wls/oracle.soa_template_12.1.3.jar')
 
+print 'updatingDomain'
 updateDomain()
+print 'updatingDomain done'
 closeDomain()
 
 print('Domain Creation Completed')
